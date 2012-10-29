e46a54c8d9f8625e24240781199b8b333ea5b027
user <user@squeeze.travelcom.michelin-travel.com>
2012-10-29 18:05:42 +0100

    Added Sf2qotd/WebsiteBundle with install notes

---
+++ Symfony/src/Sf2qotd/WebsiteBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sf2qotd\WebsiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
---
+++ Symfony/src/Sf2qotd/WebsiteBundle/Sf2qotdWebsiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sf2qotd\WebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Sf2qotdWebsiteBundle extends Bundle
+{
+}
